02808c1ec5e91fe0ec0ce2529bd0efb30fe0c800
Gilles de Hollander <Gilles.de.Hollander@gmail.com>
2016-03-16 17:54:49 +0100

    Some updates

--- pyseqsample/plotting.py
+++ pyseqsample/plotting.py
@@ -8,20 +8,20 @@ import seaborn as sns
 def plot_responses(responses, rts, bins=None, t=None, kde=True, **kwargs):
     
     if bins == None:
-        bins = np.linspace(0, 1.05 * rts.max(), len(rts)/ 15)
+        bins = np.linspace(0, 1.05 * rts.max(), np.min((len(rts)/ 15, 50)))
         
     if not t:
         t = np.linspace(0, 1.05*rts.max(), 100)
     
-    for r in np.unique(responses):
+    for i, r in enumerate(np.unique(responses)):
         idx = responses == r
         
         if kde:
             kde = sp.stats.gaussian_kde(rts[idx])
-            plt.plot(t, kde.pdf(t) * idx.mean(), color=sns.color_palette()[r-1], lw=2)
+            plt.plot(t, kde.pdf(t) * idx.mean(), color=sns.color_palette()[i], lw=2)
 
         hist, bin_edges = np.histogram(rts[idx], bins=bins, density=True)        
-        plt.bar(bin_edges[:-1], hist * (idx.mean()), width=bins[1] - bins[0], color=sns.color_palette()[r-1], alpha=0.8 )
+        plt.bar(bin_edges[:-1], hist * (idx.mean()), width=bins[1] - bins[0], color=sns.color_palette()[i], alpha=0.8 )
 
 
 def plot_quantiles(responses, rts, q=(0.1, 0.3, 0.5, 0.7, 0.9), *args, **kwargs):
@@ -29,14 +29,19 @@ def plot_quantiles(responses, rts, q=(0.1, 0.3, 0.5, 0.7, 0.9), *args, **kwargs)
     q = np.array(q)
 
     
-    for r in np.unique(responses):
+    for i, r in enumerate(np.unique(responses)):
         
                 
         xs = np.percentile(rts, q*100)
         
         ys = q * (responses == r).mean()
-        
-        plt.plot(xs, ys, c=sns.color_palette()[r-1], **kwargs)
+
+        print kwargs
+
+        if 'color' in kwargs.keys():
+            plt.plot(xs, ys, c=kwargs['color'], **kwargs)
+        else:
+            plt.plot(xs, ys, c=sns.color_palette()[i], **kwargs)
 
 
     plt.xlim(0, 1.05 * rts.max())
